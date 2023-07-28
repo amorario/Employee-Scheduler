fd7de434bfff43a8e7f8096054cb052e223b9f54
amorario <40482797+amorario@users.noreply.github.com>
2023-07-28 14:19:12 -0400

    Add files via upload

---
+++ thymeleaf-employees-main/thymeleaf-employees-main/thymeleaf-employees/doctor_directory.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE  IF NOT EXISTS `doctor_directory`;
+USE `doctor_directory`;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `employee` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `shift`;
+CREATE TABLE `shift` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `scall` varchar(45) DEFAULT NULL,
+  `date` DATETIME DEFAULT NULL,
+  `employee_id` int DEFAULT NULL,
+
+  PRIMARY KEY (`id`),
+
+  KEY `FK_EMPLOYEE_ID_idx` (`employee_id`),
+
+  CONSTRAINT `FK_EMPLOYEE` 
+  FOREIGN KEY (`employee_id`) 
+  REFERENCES `employee` (`id`) 
+
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO `employee` VALUES 
+	(1,'Leslie','Andrews','leslie@luv2code.com'),
+	(2,'Emma','Baumgarten','emma@luv2code.com'),
+	(3,'Avani','Gupta','avani@luv2code.com'),
+	(4,'Yuri','Petrov','yuri@luv2code.com'),
+	(5,'Juan','Vega','juan@luv2code.com');
+    
+INSERT INTO `shift` VALUES 
+	(11,'EARLY','2023-08-12',2),
+	(12,'LATE','2023-08-17',5),
+	(13,'MID','2023-08-03',4),
+	(14,'NO','2023-08-16',1),
+	(15,'EARLY','2023-08-15',2);
+    
---
+++ thymeleaf-employees-main/thymeleaf-employees-main/thymeleaf-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Shift.java
@@ -0,0 +1,105 @@
+package com.luv2code.springboot.thymeleafdemo.entity;
+
+import java.text.DateFormat;
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="shift")
+public class Shift {
+  
+      @Id
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      @Column(name="id")
+      private int id;
+  
+      @Column(name = "scall")
+      private String call;
+
+      //@Column(name = "date")
+      //private String date;
+      @Column(name = "date")
+      private LocalDate date;
+
+
+
+    @ManyToOne(cascade = {CascadeType.PERSIST,
+                          CascadeType.DETACH, CascadeType.REFRESH})
+      @JoinColumn(name="employee_id")
+      private Employee employee;
+
+      //DateFormatSymbols syms = new DateFormatSymbols(new Locale ("en","US"));
+      //DateFormat output = new SimpleDateFormat("MM-dd-yyyy", syms);
+      //DateFormat output = new SimpleDateFormat("EEE MMMMM dd, YYYY 'at' hh:mm aaa", syms);
+			  
+      public String color;
+  
+      public Shift () {
+      }
+
+      /*public Shift (String call, String date) {
+        this.call = call;
+        this.date = date;
+      }*/
+      public Shift (String call, LocalDate date) {
+          this.call = call;
+          this.date = date;
+      }
+      
+      public int getId() {
+	return id;
+      }
+      public void setId(int id) {
+	this.id = id;
+      }
+      public String getCall() {
+	return call;
+      }
+      public void setCall(String call) {
+	this.call = call;
+      }
+      /*public String getDate() {
+	return date;
+      }
+      public void setDate(String date) {
+	this.date = date;
+      }*/
+      public LocalDate getDate() {
+          return date;
+      }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+      public Employee getEmployee() {
+	return employee;
+      }
+      public void setEmployee (Employee employee) {
+	this.employee = employee;
+      }
+      
+      
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+  
+      @Override
+      public String toString () {
+	return "Shift{" +
+                "id=" + id +
+                ", call= " + call +
+            //", date= " + output.format(date) +
+            ", date= " + date.format(DateTimeFormatter.ofPattern("MM-dd-yyyy")) +
+            ", employeeId= " + employee.getId() +
+                '}';
+      }
+}
